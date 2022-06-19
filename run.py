0ecffc670f4c7d66b5f2a0d2797a7841794364eb
KristianColville1 <kristiancolville1@gmail.com>
2022-06-19 11:57:21 +0100

    Add views for various templates and insert urls for links

--- run.py
+++ run.py
@@ -7,8 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-# debug must be set to false when submitting projects,
-# its for production environments
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def about():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
